370310ed4de9202e42bd02fd73a7ef697382b996
AdrianHsu <adrianhsu1995@gmail.com>
2015-08-14 14:16:00 +0800

    first commit

---
+++ app/src/main/java/com/dots/test_1_uxflow/MainActivity.java
@@ -0,0 +1,18 @@
+package com.dots.test_1_uxflow;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.GridView;
+
+
+public class MainActivity extends Activity {
+
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+  }
+}
